2ddf0f6c6170e489f1283cf33597f606f322baff
Jacqueline Lee <anginblu@gmail.com>
2018-01-10 06:18:04 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < Sinatra::Base
 
   patch '/recipes/:id' do #edit action
     @recipe = Recipe.find_by_id(params[:id])
-    @recipe.name = params[:name]
+    @recipe.name = params[:name] unless params[:name] == nil
     @recipe.ingredients = params[:ingredients]
     @recipe.cook_time = params[:cook_time]
     @recipe.save
